3de9bc1a3d765adb20ec300c8c8a0d25ea8a8a69
racha <rachabnabid@gmail.com>
2022-04-07 22:28:58 +0100

    type des variables

---
+++ exercice4/exercice4.js
@@ -0,0 +1,13 @@
+var a = "1";
+var b = "2";
+var c = a+b;
+alert("le type de a est " + typeof(a)+
+"\nle type de b est " + typeof(b) + 
+"\nle type de c est " + typeof(c));
+a = parseInt();
+b = parseInt();
+c = a+b;
+alert("la valeure de c est " + c +
+"\nle type de a est " + typeof(a)+
+"\nle type de b est " + typeof(b) + 
+"\nle type de c est " + typeof(c));
