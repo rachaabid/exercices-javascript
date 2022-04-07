ad373b0b518ebda94c079101bb363a37b6dc0b33
racha <rachabnabid@gmail.com>
2022-04-07 23:39:42 +0100

    nbre aléatoire

--- exercice6/exercice6.js
+++ exercice6/exercice6.js
@@ -1,9 +1,16 @@
-var x = Math.random();
+var x = Math.random()*10;
 var i = '';
 do {
   var y = prompt("entrez un nombre");
+  if (y<x) {
+    alert("le nombre y est plus petit");
+  }
+  else if (y>x) {
+    alert("le nombre y est plus grand");}
+  else {
+    alert("le nombre est trouvé en " + i, "coups");
+  }
   i++;
-  
 }
 while (x != y)
 
